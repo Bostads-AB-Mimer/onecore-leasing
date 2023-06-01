3c81ec0272b250ebaec570d14e3d95fbaf4ae31c
Anders Norrback Bornholm <anders@bornholm.se>
2023-06-01 12:40:56 +0200

    Added unit tests

--- src/services/lease-service/tests/index.test.ts
+++ src/services/lease-service/tests/index.test.ts
@@ -3,6 +3,8 @@ import Koa from 'koa'
 import KoaRouter from '@koa/router'
 import bodyParser from 'koa-bodyparser'
 import { routes } from '../index'
+import * as tenantLeaseAdapter from '../adapters/tenant-lease-adapter'
+import leaseMock from './leaseMock'
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -12,10 +14,27 @@ app.use(router.routes())
 
 describe('lease-service', () => {
   describe('GET /leases', () => {
-    it('responds', async () => {
-      const res = await request(app.callback()).get('/my-lease')
+    it('responds with an array of leases', async () => {
+      jest.spyOn(tenantLeaseAdapter, 'getLease').mockResolvedValue(leaseMock)
+
+      const res = await request(app.callback()).get('/leases')
       expect(res.status).toBe(200)
-      expect(res.body.data.lease).toBeDefined()
+      expect(res.body.data).toBeInstanceOf(Array)
+      expect(JSON.stringify(res.body.data[0])).toEqual(
+        JSON.stringify(leaseMock)
+      )
+    })
+  })
+
+  describe('GET /leases/:id', () => {
+    it('responds with a leases', async () => {
+      jest.spyOn(tenantLeaseAdapter, 'getLease').mockResolvedValue(leaseMock)
+
+      const res = await request(app.callback()).get('/leases/1337')
+      expect(res.status).toBe(200)
+      expect(JSON.stringify(res.body.data.lease)).toEqual(
+        JSON.stringify(leaseMock)
+      )
     })
   })
 })
