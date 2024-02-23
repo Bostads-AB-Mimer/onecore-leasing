ee7d63a415f62f5c76f2ba828582f569552c5e38
Johannes <erikjohanneskarlsson@gmail.com>
2024-02-23 15:06:17 +0100

    adds some tests

--- src/services/lease-service/adapters/invoices-adapter.ts
+++ src/services/lease-service/adapters/invoices-adapter.ts
@@ -18,43 +18,54 @@ function transformFromDbInvoice(row: any): Invoice {
     expirationDate: row.expirationDate,
     debitStatus: row.debitStatus,
     paymentStatus: row.paymentStatus,
-    transactionTypeName: row.transactionTypeName.trim()
-  };
+    transactionTypeName: row.transactionTypeName.trim(),
+  }
 }
 
 const getInvoicesByContactCode = async (
-  contactKey: string,
+  contactKey: string
 ): Promise<Invoices | undefined> => {
-  const rows = await db.select(
-    'krfkh.invoice as invoiceId',
-    'krfkh.reference as leaseId',
-    'krfkh.amount as amount',
-    'krfkh.fromdate as fromDate',
-    'krfkh.todate as toDate',
-    'krfkh.invdate as invoiceDate',
-    'krfkh.expdate as expirationDate',
-    'krfkh.debstatus as debitStatus',
-    'krfkh.paystatus as paymentStatus',
-    'revrt.name as transactionTypeName', //not really needed?
-  )
+  const rows = await db
+    .select(
+      'krfkh.invoice as invoiceId',
+      'krfkh.reference as leaseId',
+      'krfkh.amount as amount',
+      'krfkh.fromdate as fromDate',
+      'krfkh.todate as toDate',
+      'krfkh.invdate as invoiceDate',
+      'krfkh.expdate as expirationDate',
+      'krfkh.debstatus as debitStatus',
+      'krfkh.paystatus as paymentStatus',
+      'revrt.name as transactionTypeName' //not really needed?
+    )
     .from('krfkh')
     .innerJoin('cmctc', 'cmctc.keycmctc', 'krfkh.keycmctc')
     .innerJoin('revrt', 'revrt.keyrevrt', 'krfkh.keyrevrt')
-    .where({ 'cmctc.cmctckod': contactKey }).orderBy('krfkh.fromdate', 'desc')
+    .where({ 'cmctc.cmctckod': contactKey })
+    .orderBy('krfkh.fromdate', 'desc')
   if (rows && rows.length > 0) {
-    const invoices: Invoice[] = rows.map(transformFromDbInvoice);
+    const invoices: Invoice[] = rows.map(transformFromDbInvoice)
 
-    if(invoices.length === 0) {
-      return undefined;
+    if (invoices.length === 0) {
+      return undefined
     }
 
-    let unpaidInvoices = invoices.filter((invoice: any) => invoice.paymentStatus === 0 || invoice.paymentStatus === 1 )
-    const paidInvoices = invoices.filter((invoice: any) => invoice.paymentStatus > 1 )
+    let unpaidInvoices = invoices.filter(
+      (invoice: any) =>
+        invoice.paymentStatus === 0 || invoice.paymentStatus === 1
+    )
+    const paidInvoices = invoices.filter(
+      (invoice: any) => invoice.paymentStatus > 1
+    )
 
-    unpaidInvoices = unpaidInvoices.filter(unpaidInvoice => {
+    unpaidInvoices = unpaidInvoices.filter((unpaidInvoice) => {
       // If a paid invoice exists with the same invoice id and amount is 0, the invoice is considered paid (probably handled by debt collector)
-      return !paidInvoices.some(paidInvoice => paidInvoice.invoiceId === unpaidInvoice.invoiceId && unpaidInvoice.amount === 0);
-    });
+      return !paidInvoices.some(
+        (paidInvoice) =>
+          paidInvoice.invoiceId === unpaidInvoice.invoiceId &&
+          unpaidInvoice.amount === 0
+      )
+    })
 
     return {
       unpaidInvoices: unpaidInvoices,
@@ -66,37 +77,37 @@ const getInvoicesByContactCode = async (
 }
 
 const getUnpaidInvoicesByContactCode = async (
-  contactKey: string,
-): Promise<UnpaidInvoices | undefined> =>  {
-  var allInvoices = await getInvoicesByContactCode(contactKey);
+  contactKey: string
+): Promise<UnpaidInvoices | undefined> => {
+  const allInvoices = await getInvoicesByContactCode(contactKey)
 
-  if(allInvoices?.unpaidInvoices == undefined) {
-    return undefined;
+  if (allInvoices?.unpaidInvoices == undefined) {
+    return undefined
   }
 
-  let accumulatedLastDebitDaysSinceToday = 0;
+  let accumulatedLastDebitDaysSinceToday = 0
 
-  let unpaidInvoices: UnpaidInvoice[] | undefined = allInvoices?.unpaidInvoices.map((invoice: Invoice) => {
-    const today = new Date();
-    const daysSinceLastDebitDate = Math.floor((today.getTime() - invoice.fromDate.getTime()) / (1000 * 60 * 60 * 24));
-    accumulatedLastDebitDaysSinceToday += daysSinceLastDebitDate;
-    return {
-      invoice: invoice.invoiceId,
-      amount: invoice.amount,
-      fromDate: invoice.fromDate,
-      toDate: invoice.toDate,
-      daysSinceLastDebitDate: daysSinceLastDebitDate,
-    }
-  })
+  const unpaidInvoices: UnpaidInvoice[] | undefined =
+    allInvoices?.unpaidInvoices.map((invoice: Invoice) => {
+      const today = new Date()
+      const daysSinceLastDebitDate = Math.floor(
+        (today.getTime() - invoice.fromDate.getTime()) / (1000 * 60 * 60 * 24)
+      )
+      accumulatedLastDebitDaysSinceToday += daysSinceLastDebitDate
+      return {
+        invoice: invoice.invoiceId,
+        amount: invoice.amount,
+        fromDate: invoice.fromDate,
+        toDate: invoice.toDate,
+        daysSinceLastDebitDate: daysSinceLastDebitDate,
+      }
+    })
 
-  return  {
+  return {
     invoices: unpaidInvoices,
     numberOfUnpaidInvoices: unpaidInvoices.length,
-    accumulatedLastDebitDaysSinceToday: accumulatedLastDebitDaysSinceToday
+    accumulatedLastDebitDaysSinceToday: accumulatedLastDebitDaysSinceToday,
   }
 }
 
-export {
-  getInvoicesByContactCode,
-  getUnpaidInvoicesByContactCode,
-}
+export { getInvoicesByContactCode, getUnpaidInvoicesByContactCode }
