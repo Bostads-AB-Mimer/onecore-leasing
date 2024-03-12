75826fa61f48547f909b315bf5146c6f049dea1a
Anders Norrback Bornholm <anders@bornholm.se>
2024-03-12 09:52:06 +0100

    Feat/1202 internal check (#21)
    * Fixed tests after reworking get invoices
    
    * Fixed tests after reworking get invoices

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/build/'],
-  moduleNameMapper: {
-    'onecore-types':
-      '<rootDir>/node_modules/onecore-types/build/types/types.d.ts',
-  },
   transformIgnorePatterns: ['node_modules/(?!(onecore-types)/)'],
   extensionsToTreatAsEsm: ['.d.ts, .ts'],
 }
