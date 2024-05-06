8d2b3fb7de64797a3f2a6d3c238ae32ba998c795
William Ryder <williamryderb@gmail.com>
2024-05-06 16:36:33 +0200

    feat: add nationalRegistrationNumber to applicants

--- src/services/lease-service/adapters/listing-adapter.ts
+++ src/services/lease-service/adapters/listing-adapter.ts
@@ -37,6 +37,7 @@ function transformDbApplicant(row: any): Applicant {
   return {
     id: row.Id,
     name: row.Name,
+    nationalRegistrationNumber: row.NationalRegistrationNumber,
     contactCode: row.ContactCode,
     applicationDate: row.ApplicationDate,
     applicationType: row.ApplicationType,
@@ -155,6 +156,7 @@ const getApplicantById = async (
 const createApplication = async (applicationData: Applicant) => {
   await db('applicant').insert({
     Name: applicationData.name,
+    NationalRegistrationNumber: applicationData.nationalRegistrationNumber,
     ContactCode: applicationData.contactCode,
     ApplicationDate: applicationData.applicationDate,
     ApplicationType: applicationData.applicationType,
