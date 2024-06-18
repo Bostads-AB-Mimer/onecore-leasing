1f975af27409f71f180e4ec84ea78f73941e8831
momentiris <momentiris@gmail.com>
2024-06-18 15:21:45 +0200

    add invoice factory

--- src/services/lease-service/tests/invoices.test.ts
+++ src/services/lease-service/tests/invoices.test.ts
@@ -15,8 +15,10 @@ import request from 'supertest'
 import Koa from 'koa'
 import KoaRouter from '@koa/router'
 import bodyParser from 'koa-bodyparser'
+
 import { routes } from '../index'
 import * as invoiceAdapter from '../adapters/xpand/invoices-adapter'
+import * as factory from './factories'
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -24,38 +26,34 @@ routes(router)
 app.use(bodyParser())
 app.use(router.routes())
 
-describe('invoices-service', () => {
-  describe('GET /getInvoicesByContactCode', () => {
-    it('responds with an object of invoices', async () => {
-      const getInvoicesSpy = jest.spyOn(
-        invoiceAdapter,
-        'getInvoicesByContactCode'
-      )
+describe('GET /getInvoicesByContactCode', () => {
+  it('responds with an object of invoices', async () => {
+    const getInvoicesSpy = jest
+      .spyOn(invoiceAdapter, 'getInvoicesByContactCode')
+      .mockResolvedValueOnce(factory.invoice.buildList(1))
 
-      const res = await request(app.callback()).get(
-        '/contact/invoices/contactCode/contactKey'
-      )
-      expect(res.status).toBe(200)
-      expect(getInvoicesSpy).toHaveBeenCalled()
-      expect(res.body.data).toBeDefined()
-      expect(res.body.data).toBeInstanceOf(Array)
-    })
+    const res = await request(app.callback()).get(
+      '/contact/invoices/contactCode/contactKey'
+    )
+    expect(res.status).toBe(200)
+    expect(getInvoicesSpy).toHaveBeenCalled()
+    expect(res.body.data).toBeDefined()
+    expect(res.body.data).toBeInstanceOf(Array)
   })
+})
 
-  describe('GET /getUnpaidInvoicesByContactCode', () => {
-    it('responds with an object of invoices', async () => {
-      const getInvoicesSpy = jest.spyOn(
-        invoiceAdapter,
-        'getUnpaidInvoicesByContactCode'
-      )
+describe('GET /getUnpaidInvoicesByContactCode', () => {
+  it('responds with an object of invoices', async () => {
+    const getInvoicesSpy = jest
+      .spyOn(invoiceAdapter, 'getUnpaidInvoicesByContactCode')
+      .mockResolvedValueOnce(factory.invoice.buildList(1))
 
-      const res = await request(app.callback()).get(
-        '/contact/unpaidInvoices/contactCode/contactKey'
-      )
-      expect(res.status).toBe(200)
-      expect(res.body.data).toBeInstanceOf(Object)
-      expect(getInvoicesSpy).toHaveBeenCalled()
-      expect(res.body.data).toBeDefined()
-    })
+    const res = await request(app.callback()).get(
+      '/contact/unpaidInvoices/contactCode/contactKey'
+    )
+    expect(res.status).toBe(200)
+    expect(res.body.data).toBeInstanceOf(Object)
+    expect(getInvoicesSpy).toHaveBeenCalled()
+    expect(res.body.data).toBeDefined()
   })
 })
