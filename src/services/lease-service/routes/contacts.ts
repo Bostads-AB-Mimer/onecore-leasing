5cfa3940280419d045815f1cf402a9859085a85c
driatic <fredrik.hasselgren@prototyp.se>
2024-07-09 10:59:27 +0200

    feat:1537 wip docs

--- src/services/lease-service/routes/contacts.ts
+++ src/services/lease-service/routes/contacts.ts
@@ -3,6 +3,16 @@ import KoaRouter from '@koa/router'
 import * as tenantLeaseAdapter from '../adapters/xpand/tenant-lease-adapter'
 
 export const routes = (router: KoaRouter) => {
+  /**
+   * @api {get} /user/:id Request User information
+   * @apiName GetUser
+   * @apiGroup User
+   *
+   * @apiParam {Number} id User's unique ID.
+   *
+   * @apiSuccess {String} firstname Firstname of the User.
+   * @apiSuccess {String} lastname  Lastname of the User.
+   */
   router.get('(.*)/contacts/search', async (ctx) => {
     if (typeof ctx.query.q !== 'string') {
       ctx.status = 400
--- src/services/lease-service/routes/offers.ts
+++ src/services/lease-service/routes/offers.ts
@@ -5,6 +5,7 @@ import { z } from 'zod'
 
 import * as offerAdapter from './../adapters/offer-adapter'
 import { parseRequestBody } from '../../../middlewares/parse-request-body'
+import swaggerJsdoc from 'swagger-jsdoc'
 
 export const routes = (router: KoaRouter) => {
   const createOfferRequestParams = z.object({
@@ -15,6 +16,15 @@ export const routes = (router: KoaRouter) => {
     applicantId: z.number(),
   })
 
+  /**
+   * @swagger
+   * /offer:
+   *   get:
+   *     description: Welcome to swagger-jsdoc!
+   *     responses:
+   *       200:
+   *         description: Returns a mysterious string.
+   */
   router.post(
     '(.*)/offer',
     parseRequestBody(createOfferRequestParams),
@@ -30,4 +40,27 @@ export const routes = (router: KoaRouter) => {
       }
     }
   )
+
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Hello World',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./src/services/lease-service/routes/*.ts'],
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  }
+
+  const swaggerSpec = swaggerJsdoc(options)
+
+  router.get('/swagger.json', async function (ctx) {
+    ctx.set('Content-Type', 'application/json')
+    ctx.body = swaggerSpec
+  })
 }
