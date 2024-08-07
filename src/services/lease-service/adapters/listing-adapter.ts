72837134040925c17681ce474554a292f3a31b6d
momentiris <momentiris@gmail.com>
2024-08-07 11:48:00 +0200

    add types for getting listing(s)

--- src/services/lease-service/adapters/listing-adapter.ts
+++ src/services/lease-service/adapters/listing-adapter.ts
@@ -1,3 +1,5 @@
+import { logger } from 'onecore-utilities'
+
 import {
   Applicant,
   Listing,
@@ -5,34 +7,52 @@ import {
   ListingStatus,
 } from 'onecore-types'
 import { db } from './db'
-import { logger } from 'onecore-utilities'
+import { DbApplicant } from './types'
 
-function transformFromDbListing(row: any): Listing {
-  // TODO: Listing has some properties T | undefined.
-  // However, this function is returning null when the db returns null
+type DbListing = {
+  Id: number
+  RentalObjectCode: string
+  Address: string
+  MonthlyRent: number
+  DistrictCaption: string | null
+  DistrictCode: string | null
+  BlockCaption: string | null
+  BlockCode: string | null
+  ObjectTypeCaption: string | null
+  ObjectTypeCode: string | null
+  RentalObjectTypeCaption: string | null
+  RentalObjectTypeCode: string | null
+  PublishedFrom: Date
+  PublishedTo: Date
+  VacantFrom: Date
+  Status: ListingStatus
+  WaitingListType: string | null
+}
+
+function transformFromDbListing(row: DbListing): Listing {
   return {
     id: row.Id,
     rentalObjectCode: row.RentalObjectCode,
     address: row.Address,
     monthlyRent: row.MonthlyRent,
-    districtCaption: row.DistrictCaption,
-    districtCode: row.DistrictCode,
-    blockCaption: row.BlockCaption,
-    blockCode: row.BlockCode,
-    objectTypeCaption: row.ObjectTypeCaption,
-    objectTypeCode: row.ObjectTypeCode,
-    rentalObjectTypeCaption: row.RentalObjectTypeCaption,
-    rentalObjectTypeCode: row.RentalObjectTypeCode,
-    publishedFrom: row.PublishedFrom,
+    districtCaption: row.DistrictCaption || undefined,
+    districtCode: row.DistrictCode || undefined,
+    blockCaption: row.BlockCaption || undefined,
+    blockCode: row.BlockCode || undefined,
+    objectTypeCaption: row.ObjectTypeCaption || undefined,
+    objectTypeCode: row.ObjectTypeCode || undefined,
+    rentalObjectTypeCaption: row.RentalObjectTypeCaption || undefined,
+    rentalObjectTypeCode: row.RentalObjectTypeCode || undefined,
+    publishedFrom: row.PublishedFrom || undefined,
     publishedTo: row.PublishedTo,
     vacantFrom: row.VacantFrom,
     status: row.Status,
-    waitingListType: row.WaitingListType,
+    waitingListType: row.WaitingListType || undefined,
     applicants: undefined,
   }
 }
 
-function transformDbApplicant(row: any): Applicant {
+function transformDbApplicant(row: DbApplicant): Applicant {
   return {
     id: row.Id,
     name: row.Name,
@@ -46,7 +66,7 @@ function transformDbApplicant(row: any): Applicant {
 }
 
 const createListing = async (listingData: Listing) => {
-  const insertedRow = await db('Listing')
+  const insertedRow = await db<DbListing>('Listing')
     .insert({
       RentalObjectCode: listingData.rentalObjectCode,
       Address: listingData.address,
@@ -79,7 +99,7 @@ const createListing = async (listingData: Listing) => {
 const getListingByRentalObjectCode = async (
   rentalObjectCode: string
 ): Promise<Listing | undefined> => {
-  const listing = await db('Listing')
+  const listing = await db<DbListing>('Listing')
     .where({
       RentalObjectCode: rentalObjectCode,
     })
@@ -91,19 +111,13 @@ const getListingByRentalObjectCode = async (
   return transformFromDbListing(listing)
 }
 
-/**
- * Checks if a listing already exists based on unique criteria.
- *
- * @param {string} listingId - The rental object code of the listing (originally from xpand).
- * @returns {Promise<Listing>} - Promise that resolves to the existing listing if it exists.
- */
 const getListingById = async (
   listingId: string
 ): Promise<Listing | undefined> => {
   logger.info({ listingId }, 'Getting listing from leasing DB')
   const result = await db
     .from('listing AS l')
-    .select(
+    .select<DbListing & { applicants: string | null }>(
       'l.*',
       db.raw(`
       (
@@ -117,14 +131,21 @@ const getListingById = async (
     .where('l.Id', listingId)
     .first()
 
-  const parseApplicantsJson = (row: { applicants?: string }) => ({
-    ...row,
-    applicants: row.applicants ? JSON.parse(row.applicants) : [],
+  const parseApplicantsJson = (applicants: string | null) =>
+    applicants ? JSON.parse(applicants) : []
+
+  const parseApplicantsApplicationDate = (applicant: Applicant): Applicant => ({
+    ...applicant,
+    applicationDate: new Date(applicant.applicationDate),
   })
 
-  const transformListing = (row: { applicants: Array<unknown> }): Listing => ({
+  const transformListing = (
+    row: DbListing & { applicants: Array<DbApplicant> }
+  ): Listing => ({
     ...transformFromDbListing(row),
-    applicants: row.applicants.map(transformDbApplicant),
+    applicants: row.applicants
+      .map(transformDbApplicant)
+      .map(parseApplicantsApplicationDate),
   })
 
   if (!result) {
@@ -137,7 +158,10 @@ const getListingById = async (
 
   logger.info({ listingId }, 'Getting listing from leasing DB complete')
 
-  return transformListing(parseApplicantsJson(result))
+  return transformListing({
+    ...result,
+    applicants: parseApplicantsJson(result.applicants),
+  })
 }
 
 /**
@@ -214,12 +238,7 @@ const updateApplicantStatus = async (
   }
 }
 
-/**
- * Gets all listings with applicants
- *
- * @returns {Promise<Listing[]>} - Returns a list of listings.
- */
-const getAllListingsWithApplicants = async () => {
+const getAllListingsWithApplicants = async (): Promise<Array<Listing>> => {
   const query = `
     SELECT
       l.*,
@@ -232,19 +251,33 @@ const getAllListingsWithApplicants = async () => {
     FROM listing l
   `
 
-  const parseApplicantsJson = (row: { applicants?: string }) => ({
-    ...row,
-    applicants: row.applicants ? JSON.parse(row.applicants) : [],
+  const parseApplicantsJson = (applicants: string | null) =>
+    applicants ? JSON.parse(applicants) : []
+
+  const parseApplicantsApplicationDate = (applicant: Applicant): Applicant => ({
+    ...applicant,
+    applicationDate: new Date(applicant.applicationDate),
   })
 
-  const transformListing = (row: { applicants: Array<unknown> }) => ({
+  const transformListing = (
+    row: DbListing & { applicants: Array<DbApplicant> }
+  ): Listing => ({
     ...transformFromDbListing(row),
-    applicants: row.applicants.map(transformDbApplicant),
+    applicants: row.applicants
+      .map(transformDbApplicant)
+      .map(parseApplicantsApplicationDate),
   })
 
-  const result: Array<Listing> = await db
-    .raw(query)
-    .then((rows) => rows.map(parseApplicantsJson).map(transformListing))
+  const result = await db
+    .raw<Array<DbListing & { applicants: string | null }>>(query)
+    .then((rows) =>
+      rows.map((row) => {
+        return transformListing({
+          ...row,
+          applicants: parseApplicantsJson(row.applicants),
+        })
+      })
+    )
 
   return result
 }
