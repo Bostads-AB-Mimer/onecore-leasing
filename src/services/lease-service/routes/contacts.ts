5c5f872e586af4adbc338b5ae5968ea942592765
momentiris <momentiris@gmail.com>
2024-11-06 11:47:02 +0100

    typo

--- src/services/lease-service/routes/contacts.ts
+++ src/services/lease-service/routes/contacts.ts
@@ -449,8 +449,7 @@ export const routes = (router: KoaRouter) => {
    *         required: true
    *         schema:
    *           type: string
-   *         description: The contact code associated with the application
-   *         profile.
+   *         description: The contact code associated with the application profile.
    *     responses:
    *       200:
    *         description: Successfully retrieved application profile.
