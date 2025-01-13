637156e247ed3429f6100c07335d47f19e23ea92
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2025-01-13 14:39:31 +0100

    UTH-164: review status and housing type types (#187)
    * review status and housing type are now enums
    
    * migrate altered housing reference changes
    
    * adapt to migration  and onecore-types changes
    
    * housing reference not optional anymore
    
    * lastUpdatedby is not part of the payloads for now
    
    * actually update all relevant housing reference columns
    
    * also dont allow null on application profile housing type
    
    * make adapter params strict to schema type
    
    * wip
    
    * unify create and update profile and housing reference into profile adapter
    
    * use right expires at value
    
    * leasing uses its own schemas for database adapters
    
    * update tests to use db context
    
    * bump onecore-types

--- src/services/lease-service/update-or-create-application-profile.ts
+++ src/services/lease-service/update-or-create-application-profile.ts
@@ -1,14 +1,9 @@
-import {
-  ApplicationProfile,
-  ApplicationProfileHousingReference,
-  leasing,
-} from 'onecore-types'
+import { ApplicationProfile, leasing } from 'onecore-types'
 import { Knex } from 'knex'
 import { z } from 'zod'
 
 import { AdapterResult } from './adapters/types'
 import * as applicationProfileAdapter from './adapters/application-profile-adapter'
-import * as applicationProfileHousingReferenceAdapter from './adapters/application-profile-housing-reference-adapter'
 
 type Params = z.infer<
   typeof leasing.CreateOrUpdateApplicationProfileRequestParamsSchema
@@ -19,111 +14,26 @@ export async function updateOrCreateApplicationProfile(
   contactCode: string,
   params: Params
 ): Promise<
-  AdapterResult<
-    [ApplicationProfile, 'created' | 'updated'],
-    | 'update-profile'
-    | 'update-reference'
-    | 'create-profile'
-    | 'create-reference'
-    | 'unknown'
-  >
+  AdapterResult<['created' | 'updated', ApplicationProfile], 'unknown'>
 > {
-  const trx = await db.transaction()
+  const update = await applicationProfileAdapter.update(db, contactCode, params)
 
-  const profileResult = await updateOrCreateProfile(trx, contactCode, params)
-  if (!profileResult.ok) {
-    await trx.rollback()
-    return { ok: false, err: profileResult.err }
-  }
-
-  const [profile, operation] = profileResult.data
-  if (!params.housingReference) {
-    await trx.commit()
-    return { ok: true, data: profileResult.data }
-  }
-
-  const housingReferenceResult = await updateOrCreateReference(trx, {
-    ...params.housingReference,
-    applicationProfileId: profile.id,
-  })
-
-  if (!housingReferenceResult.ok) {
-    await trx.rollback()
-    return { ok: false, err: housingReferenceResult.err }
-  }
-
-  const [housingReference] = housingReferenceResult.data
-
-  await trx.commit()
-  return { ok: true, data: [{ ...profile, housingReference }, operation] }
-}
-
-async function updateOrCreateProfile(
-  trx: Knex,
-  contactCode: string,
-  params: Params
-): Promise<
-  AdapterResult<
-    [ApplicationProfile, 'created' | 'updated'],
-    'update-profile' | 'create-profile'
-  >
-> {
-  const updateProfile = await applicationProfileAdapter.update(
-    trx,
-    contactCode,
-    params
-  )
-
-  if (!updateProfile.ok) {
-    if (updateProfile.err !== 'no-update') {
-      return { ok: false, err: 'update-profile' }
+  if (!update.ok) {
+    if (update.err !== 'no-update') {
+      return { ok: false, err: 'unknown' }
     }
-
-    const insertProfile = await applicationProfileAdapter.create(trx, {
+    const profile = await applicationProfileAdapter.create(
+      db,
       contactCode,
-      ...params,
-    })
-
-    if (!insertProfile.ok) {
-      return { ok: false, err: 'create-profile' }
-    }
-
-    return { ok: true, data: [insertProfile.data, 'created'] }
-  }
-
-  return { ok: true, data: [updateProfile.data, 'updated'] }
-}
-
-async function updateOrCreateReference(
-  trx: Knex,
-  params: Params['housingReference'] & { applicationProfileId: number }
-): Promise<
-  AdapterResult<
-    [ApplicationProfileHousingReference, 'created' | 'updated'],
-    'update-reference' | 'create-reference'
-  >
-> {
-  const updateReference =
-    await applicationProfileHousingReferenceAdapter.update(
-      trx,
-      params.applicationProfileId,
       params
     )
 
-  if (!updateReference.ok) {
-    if (updateReference.err !== 'no-update') {
-      return { ok: false, err: 'update-reference' }
+    if (!profile.ok) {
+      return { ok: false, err: 'unknown' }
     }
 
-    const insertReference =
-      await applicationProfileHousingReferenceAdapter.create(trx, params)
-
-    if (!insertReference.ok) {
-      return { ok: false, err: 'create-reference' }
-    }
-
-    return { ok: true, data: [insertReference.data, 'created'] }
+    return { ok: true, data: ['created', profile.data] }
   }
 
-  return { ok: true, data: [updateReference.data, 'updated'] }
+  return { ok: true, data: ['updated', update.data] }
 }
