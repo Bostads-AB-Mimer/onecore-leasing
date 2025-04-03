2985d224d8e0764cc30c162304326c2c43e851e6
Sven Johansson <sven.johansson@iteam.se>
2025-04-03 09:28:12 +0200

    chore: Fixed typo

--- src/api.ts
+++ src/api.ts
@@ -3,13 +3,13 @@ import KoaRouter from '@koa/router'
 import { routes as leaseRoutes } from './services/lease-service'
 import { routes as creditSafeRoutes } from './services/creditsafe'
 import { routes as healthRoutes } from './services/health-service'
-import { routes as swagggerRoutes } from './services/swagger'
+import { routes as swaggerRoutes } from './services/swagger'
 
 const router = new KoaRouter()
 
 leaseRoutes(router)
 creditSafeRoutes(router)
 healthRoutes(router)
-swagggerRoutes(router)
+swaggerRoutes(router)
 
 export default router
