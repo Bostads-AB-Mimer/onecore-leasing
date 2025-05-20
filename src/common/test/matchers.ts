aa863626f214df77da301d0296d7c7c13bfbc0ca
Sven Johansson <sven.johansson@iteam.se>
2025-05-20 11:44:13 +0200

    UTH-171: Suppress linter error

--- src/common/test/matchers.ts
+++ src/common/test/matchers.ts
@@ -21,6 +21,7 @@ expect.extend({
 })
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
       toBeSameDayAs(expected: Date): R
