5f5bd94e87709b60a882a8850eb6f87c3ad9e21c
svjson <johansson.sven@gmail.com>
2025-08-11 11:53:48 +0200

    Use service connection pools for health checks + db pool configuration (#248)

--- src/services/lease-service/adapters/db.ts
+++ src/services/lease-service/adapters/db.ts
@@ -6,6 +6,12 @@ export const createDbClient = () =>
   knex({
     client: 'mssql',
     connection: Config.leasingDatabase,
+    pool: {
+      min: 2,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+    },
   })
 
 export const db = createDbClient()
--- src/services/lease-service/adapters/xpand/xpandDb.ts
+++ src/services/lease-service/adapters/xpand/xpandDb.ts
@@ -6,6 +6,12 @@ export const createXpandDbClient = () =>
   knex({
     client: 'mssql',
     connection: Config.xpandDatabase,
+    pool: {
+      min: 2,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+    },
   })
 
 export const xpandDb = createXpandDbClient()
